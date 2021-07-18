37f045ab699456f1ec5d3ddcc6a54f0b28fe3755
Administrator <274337383@qq.com>
2021-07-19 02:55:02 +0800

    first_master

---
+++ src/com/fish/java/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.fish.java;
+// public表示公开的
+// class表示定义一个类
+// HelloWorld表示一个类名
+public class HelloWorld {
+
+    /*
+        public表示公开的
+        static表示静态的
+        void表示空
+        main表示方法名是main
+        (String[] args) 是一个main方法的形式参数列表
+
+        需要记住的:
+            是一个程序的主方法,是程序的执行入口
+            是sun公司规定的,固定编写方式
+    */
+
+    public static void main(String[] args) { //表示定义一个公开的类,起名HelloWorld
+
+        //方法体
+        //方法体
+        //方法体
+        //方法体
+        //方法体
+
+
+        //java语句[java语句以";"终止，分号必须是半角分号]
+        System.out.println("Hello World!");
+
+    }
+
+}
