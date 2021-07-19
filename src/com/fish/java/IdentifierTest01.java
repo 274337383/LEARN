336a5217871b3b71bfa08895f5c54e5c4537d585
Administrator <274337383@qq.com>
2021-07-19 13:29:12 +0800

    first_master

---
+++ src/com/fish/java/IdentifierTest01.java
@@ -0,0 +1,7 @@
+package com.fish.java;
+
+public class IdentifierTest01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- src/com/fish/java/K.java
+++ src/com/fish/java/K.java
@@ -1,16 +1,29 @@
 package com.fish.java;
 
 class B {
+    public static void main(String[] args) {
+        System.out.println("Bclass");
+    }
 }
 
 class X {
-
+    public static void main(String[] args) {
+        System.out.println("Xclass");
+    }
 }
 
 class Y {
-
+    public static void main(String[] args) {
+        System.out.println("Yclass");
+    }
 }
 
 class Z {
-
+    public static void main(String[] args) {
+        System.out.println("Zclass");
+    }
 }
+/*
+public class K {
+
+}*/
