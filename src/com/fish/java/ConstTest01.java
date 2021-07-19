65721b366eb704961b205e8e0586780a76e7b788
Administrator <274337383@qq.com>
2021-07-19 21:58:03 +0800

    first_master

--- src/com/fish/java/ConstTest01.java
+++ src/com/fish/java/ConstTest01.java
@@ -13,14 +13,28 @@ package com.fish.java;
         * 字面值是java源程序的组成部分之一. 包括标识符和关键字它们都是java源程序的组成部分
 
         * 数据在现实世界当中是分门别类的,所以数据在计算机编程语言当中也是有类型的:[数据类型]
-            - 10/100 属于整数型字面值
-            - 3.14 属于浮点型字面值
-            - true、false 属于布尔型字面值
-            - "abc" , "中国人" 属于字符串型字面值
-            - 'A' 、 '人' 属于字符型字面值
+            - 10/100             属于整数型字面值
+            - 3.14               属于浮点型字面值
+            - true , false       属于布尔型字面值
+            - "abc" , "中国人"    属于字符串型字面值
+            - 'A' , '人' 属于字符型字面值
 */
 public class ConstTest01 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        System.out.println("你好吗");
+
+        System.out.println(10);
+
+        System.out.println(100);
+
+        System.out.println(false);
+
+        System.out.println(true);
+
+        System.out.println('A');
+
+        System.out.println(3.14);
     }
 }
