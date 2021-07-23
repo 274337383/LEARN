c4d9be4b0c8b668e8bfecf301deb1f2d30ea5685
Administrator <274337383@qq.com>
2021-07-23 13:24:49 +0800

    first_master

--- src/com/fish/java/VarTest03.java
+++ src/com/fish/java/VarTest03.java
@@ -1,12 +1,18 @@
 package com.fish.java;
 
 public class VarTest03 {
+    static int k = 210;
     public static void main(String[] args) {
         int i = 100;
-        System.out.println(i);
+        int a;
+        for (a=0;a<10;a++) {
+
+        }
+        System.out.println(k);
+        System.out.println(a);
     }
 
-    public static void dosome(String[] args) {
-        System.out.println(i);
+    public static void dosome() {
+        System.out.println(k);
     }
 }
