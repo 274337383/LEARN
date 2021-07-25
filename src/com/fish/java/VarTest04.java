3a77580c4738295d95063a562c15c729df0cfd7d
Administrator <274337383@qq.com>
2021-07-25 16:21:28 +0800

    first_master

--- src/com/fish/java/VarTest04.java
+++ src/com/fish/java/VarTest04.java
@@ -1,4 +1,19 @@
 package com.fish.java;
 
 public class VarTest04 {
+
+    int k = 200;
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        System.out.println(i);
+
+    }
+
+    int i = 100;
+
+    public static void dosome() {
+        int i = 90;
+    }
 }
