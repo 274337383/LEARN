ac68264a682131597fd846b77eac567f4778849e
w274337383 <274337383@qq.com>
2021-09-16 16:00:44 +0800

    ubuntu(school)

--- src/com/fish/java/KeyBroad_Input2.java
+++ src/com/fish/java/KeyBroad_Input2.java
@@ -7,22 +7,24 @@ public class KeyBroad_Input2 {
     public static void main(String[] args) {
         java.util.Scanner s = new java.util.Scanner(System.in);
 
-        String[] tianqiArray = {"下雨","晴天","寒冷","舒适","炎热"},
+        String[] tianqiArray = {"下雨","晴天","寒冷","舒适","炎热","大黑伞","小花伞","晴天"},
         sexArray = {"男","女"};
-        String tianqi, sex, wendupj;
+        String tianqi = null, sex, wendupj,san;
+        double wendu = 0.0;
+
 
         for (int i = 0; i < 3; i++) {
             System.out.print("是否下雨(y/n):");
             String a = s.next();
-            boolean b = false;
             if (Objects.equals(a, "y") || Objects.equals(a, "Y")) {
-                b = true;
+                tianqi = tianqiArray[0];
                 break;
             } else if (Objects.equals(a, "n") || Objects.equals(a, "N")) {
+                tianqi = tianqiArray[7];
                 break;
             } else if (i == 3) {
                 System.out.println("找茬是吧");
-
+                tianqi = "unknown";
                 break;
             }
             else {
@@ -32,7 +34,7 @@ public class KeyBroad_Input2 {
         }
         for (int j = 0; j < 3; j++) {
             System.out.print("温度:");
-            double wendu = Double.parseDouble(s.next());
+            wendu = Double.parseDouble(s.next());
             if (wendu < -60 || wendu > 60) {
                 System.out.println("您住火星?");
                 continue;
@@ -54,12 +56,33 @@ public class KeyBroad_Input2 {
         String a = s.next();
 
         if (Objects.equals(a, "男")) {
-
+            sex = sexArray[0];
         } else if (Objects.equals(a, "女")){
-
+            sex = sexArray[1];
         } else {
             System.out.println("外星人？");
+            sex = "unknown";
+            san = "unknown";
         }
+        if (Objects.equals(tianqi, "下雨")) {
+            if (sex.equals("男")) {
+                san = tianqiArray[5];
+            } else if (sex.equals("女")) {
+                san = tianqiArray[6];
+            }
+        } else {
+            san = "unknown";
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println("信息:");
+        System.out.println("\t性别:"+sex);
+        System.out.println("\t天气:"+tianqi);
+        // System.out.println("\t伞:"+san);
+        System.out.println("\t温度:"+wendu);
 
         s.close();
     }
