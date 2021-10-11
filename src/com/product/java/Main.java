cad8ff2f4736414b0f9e2a3f4364b33a3b69f2b4
w274337383 <274337383@qq.com>
2021-10-11 09:13:27 +0800

    Changes

--- src/com/product/java/Main.java
+++ src/com/product/java/Main.java
@@ -1,4 +1,7 @@
 package com.product.java;
 
 public class Main {
+    public static void main(String[] args) {
+        
+    }
 }
---
+++ src/com/product/java/Print.java
@@ -0,0 +1,50 @@
+package com.product.java;
+
+public class Print {
+    public static void out(byte a) {
+        System.out.print(a);
+    }
+    public static void out(short a) {
+        System.out.print(a);
+    }
+    public static void out(int a) {
+        System.out.print(a);
+    }
+    public static void out(float a) {
+        System.out.print(a);
+    }
+    public static void out(double a) {
+        System.out.print(a);
+    }
+    public static void out(boolean a) {
+        System.out.print(a);
+    }
+    public static void out(char a) {
+        System.out.print(a);
+    }
+    public static void out(String a) {
+        System.out.print(a);
+    }
+    public static void outln(byte a) {
+        System.out.println(a);
+    }
+    public static void outln(short a) {
+        System.out.println(a);
+    }
+    public static void outln(int a) {
+        System.out.println(a);
+    }
+    public static void outln(float a) {
+        System.out.println(a);
+    }
+    public static void outln(double a) {
+        System.out.println(a);
+    }
+    public static void outln(boolean a) {
+        System.out.println(a);
+    }
+    public static void outln(char a) {
+        System.out.println(a);
+    }
+    public static void outln(String a) {System.out.println(a);}
+}
