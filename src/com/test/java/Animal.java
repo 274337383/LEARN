ddfffa1053fd5874de7396871a6bd374295349c2
w274337383 <274337383@qq.com>
2021-10-25 18:17:55 +0800

    changes

--- src/com/test/java/Animal.java
+++ src/com/test/java/Animal.java
@@ -1,6 +1,12 @@
 package com.test.java;
 
+import java.util.Date;
+
 public class Animal {
+
+    public Date birthDay;
+
+
     public void move() {
         System.out.println("移动");
     }
---
+++ src/com/test1/java/User.java
@@ -0,0 +1,9 @@
+package com.test1.java;
+
+public class User {
+    // 受保护的
+    protected int i = 10;
+
+    // 缺省的
+    int j = 20;
+}
